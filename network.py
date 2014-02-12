26e86406b663a12931f6bc0bf8218fd6ef65a95c
Przemek <pastuszka.przemyslaw@gmail.com>
2014-02-12 10:38:55 +0100

    add activation function for hidden layer to the network

--- network.py
+++ network.py
@@ -5,6 +5,7 @@ Created on Feb 12, 2014
 '''
 
 from itertools import imap
+from math import tanh
 import operator
 
 class Network:
@@ -12,6 +13,7 @@ class Network:
         self.inputs = inputs
         self.hidden = hidden
         self.outputs = outputs
+        self.identity = lambda x: x
     
     def set_params(self, params):
         self.params = params
@@ -21,14 +23,14 @@ class Network:
     
     def activate(self, inp):
         param_iterator = iter(self.params)
-        hidden_layer_value = self._calculate_layer(inp, self.hidden, param_iterator)
-        output = self._calculate_layer(hidden_layer_value, self.outputs, param_iterator)
+        hidden_layer_value = self._calculate_layer(inp, self.hidden, param_iterator, tanh)
+        output = self._calculate_layer(hidden_layer_value, self.outputs, param_iterator, self.identity)
         return output
     
-    def _calculate_layer(self, previous, m, param_iterator):
+    def _calculate_layer(self, previous, m, param_iterator, activation):
         previous.append(1)
         
-        return [self._calculate_neuron(previous, param_iterator) for _ in xrange(m)]
+        return [self._calculate_neuron(previous, param_iterator, activation) for _ in xrange(m)]
     
-    def _calculate_neuron(self, previous_layer, param_iterator):
-        return sum(imap(operator.mul, iter(previous_layer), param_iterator))
+    def _calculate_neuron(self, previous_layer, param_iterator, activation):
+        return activation(sum(imap(operator.mul, iter(previous_layer), param_iterator)))
